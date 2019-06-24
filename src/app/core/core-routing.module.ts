73eb93819778dc1edd3571784a2b359e07fbf6dd
Piotr Dolny <piotr.dolny@gmail.com>
2019-06-24 15:03:03 +0200

    page 404 and bootstrap styles

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,7 +1,22 @@
+//blok angularowy
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//blok lokalnych zaleznosci
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { componentFactoryName } from '@angular/compiler';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent
+  },
+  {
+    path: "**",
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
