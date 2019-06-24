bfd60834f50ac5bd203998c8969fbc4d57a0fc4c
Piotr Dolny <piotr.dolny@gmail.com>
2019-06-24 16:04:50 +0200

    create HomePage, NotFoundPage, PostModule

--- src/app/core/pages/home-page/home-page.component.ts
+++ src/app/core/pages/home-page/home-page.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  allPosts= [ 
+    {
+    id: "jk254624",
+    body: 'Mam fajnego psa!',
+    author: {
+      name: 'Ciocia Krysia'
+    },
+    images: [
+      "https://picsum.photos/id/1/200/50"
+    ]
+  },
+
+  {
+    id: "jk252524",
+    body: 'Mam fajnego kota!',
+    author: {
+      name: 'Wujek Robert'
+    },
+    images: [
+      "https://picsum.photos/200/50"
+    ]
+  }
+]
+
   constructor() { }
 
   ngOnInit() {
