1e85d11aba10159b5457c95fc7caea26b6f8624a
Piotr Dolny <piotr.dolny@gmail.com>
2019-06-25 12:22:25 +0200

    add interfaces for posts

--- src/app/shared/services/posts.service.spec.ts
+++ src/app/shared/services/posts.service.spec.ts
@@ -2,53 +2,63 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PostsService } from './posts.service';
+import { IPostList } from '../interfaces/post-list.interface';
 
 describe('PostsService', () => {
 
-  let service: PostsService;
-  let httpMock: HttpTestingController;
+    let service: PostsService;
+    let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      //
-      imports: [
-        HttpClientTestingModule
-      ]
-    });
-    service = TestBed.get(PostsService);
-    httpMock = TestBed.get(HttpTestingController);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-    console.log('beforeEach()');
-  });
-
-  describe('getPosts', () => {
-    it('should contain method "getPosts"', () => {
-      expect(service.getPosts).toEqual(
-        jasmine.any(Function)
-      );
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            //
+            imports: [
+                HttpClientTestingModule
+            ]
+        });
+        service = TestBed.get(PostsService);
+        httpMock = TestBed.get(HttpTestingController);
     });
 
-    
-    it('should make HTTP request"', async () => {
-      //1. zarejestrowac request
-      const response = service.getPosts(); // PROMISE
-
-      //2. Strowzyc serwer HTTP, ktory bedzie zwracal mockowe dane
-      const server = httpMock.expectOne('assets/posts.json');
-      server.flush(['pierwszy', 'drugi']);
-
-      //3. Pobrac dane z odpowiedzie z servera
-      const posts = await response; // 
-
-      //4 Sprawdzic poprawnosc otrzymanych danych
-      expect(posts).toEqual(['pierwszy', 'drugi']);
-
-
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+        console.log('beforeEach()');
     });
 
-  });
+    describe('getPosts', () => {
+        it('should contain method "getPosts"', () => {
+            expect(service.getPosts).toEqual(
+                jasmine.any(Function)
+            );
+        });
+
+
+        it('should make HTTP request"', async () => {
+            //1. zarejestrowac request
+            const response = service.getPosts(); // PROMISE
+
+            //2. Strowzyc serwer HTTP, ktory bedzie zwracal mockowe dane
+            const server = httpMock.expectOne('assets/posts.json');
+
+            const fakePostList = [
+                { id: 'hfa321' },
+                { id: '1213hf' },
+            ] as IPostList;
+
+            server.flush({
+                posts: fakePostList
+            });
+
+            //3. Pobrac dane z odpowiedzie z servera
+            const posts = await response; // 
+
+
+            //4 Sprawdzic poprawnosc otrzymanych danych
+            expect(posts).toEqual(fakePostList);
+
+
+        });
+
+    });
 
 });
--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IResponsePosts } from '../interfaces/response/response-posts.interface'
+import { IPostList } from '../interfaces/post-list.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor( private http: HttpClient ) {
-   }
+  constructor(private http: HttpClient) {
+  }
 
-  getPosts() {
-    return this.http.get('assets/posts.json').toPromise();
+  async getPosts() :Promise<IPostList> {
+    const response = await this.http.get<IResponsePosts>('assets/posts.json').toPromise();
+    
+    return response.posts;
+
   }
 }
